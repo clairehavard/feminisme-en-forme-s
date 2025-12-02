f4923b01970b817fb6245fdd40ca18b29306cb37
Claire Hache <75484695+claire-havard@users.noreply.github.com>
2025-12-02 19:58:01 +0100

    Create header.js

---
+++ _assets/js/header.js
@@ -0,0 +1,31 @@
+window.addEventListener("DOMContentLoaded", function () {
+	// Ouverture menu burger
+	const menuBurger = document.querySelector("#menu-burger");
+	const mainMenu   = document.querySelector(".menu");
+	const pictoBurger = document.querySelector(".picto.burger");
+	const pictoCross  = document.querySelector(".picto.cross");
+
+	menuBurger.addEventListener("click", () => {
+		const isOpen = mainMenu.classList.toggle("visible");
+		
+		pictoBurger.classList.toggle("actived", !isOpen);
+		pictoCross.classList.toggle("actived",  isOpen);
+
+		// Fermer le sous-menu en même temps
+		if (!isOpen) {
+			subMenu.classList.remove("visible");
+			icone.classList.remove("opened");
+		}
+	});
+
+
+	// Ouverture sous-menu Slogan
+	const slogan = document.querySelector("#manifester");
+	const subMenu = document.querySelector(".sub-menu");
+	const icone = document.querySelector("#manifester .icone");
+
+	slogan.addEventListener("click", () => {
+		subMenu.classList.toggle("visible");
+		icone.classList.toggle("opened");
+	});
+});
